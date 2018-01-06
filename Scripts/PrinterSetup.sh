a677454087d814686349caee0f942bda4a2bedf0
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2018-01-06 03:30:03 -0700

    Fix samsung color profile setup + switch to cecho/cbecho

--- Scripts/PrinterSetup.sh
+++ Scripts/PrinterSetup.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-source includes/colordefines.sh
+# find script location so we can get includes
+SCRIPTSLOC=$(dirname "$0")
+INCLUDESLOC="$SCRIPTSLOC/includes"
 
-packages="cups gtk3-print-backends cups-pdf foomatic-d-engine gutenprint ghostscript gsfonts hplip splix foomatic-db-nonfree-ppds"
+source "$INCLUDESLOC/colordefines.sh"
+
+packages="cups gtk3-print-backends cups-pdf foomatic-db-engine gutenprint ghostscript gsfonts hplip splix foomatic-db-nonfree-ppds"
 packagesDEPS="foomatic-db foomatic-db-ppds foomatic-db-gutenprint-ppds libusb"
-echo -e "$CYAN Getting cups, foomatic, hp, and (older(?)) samsung drivers from normal repositories $RESET"
+cecho "Getting cups, foomatic, hp, and (older(?)) samsung drivers from normal repositories"
 sudo pacman -S $packages --needed --noconfirm
-echo -e "$CYAN Getting optional deps $RESET"
+cecho "Getting optional deps"
 sudo pacman -S --asdeps $packagesDEPS --needed --noconfirm
 
 
-echo -e "$CYAN Installing color profiles for splix (samsung printers) $RESET"
+cecho "Installing color profiles for splix (samsung printers)"
 samsungdir="/usr/share/cups/profiles/samsung"
-if [ ! -d "$samsungdir" ]; then
-    mkdir $samsungdir
-fi
-curl http://splix.ap2c.org/samsung_cms.tar.bz2 | sudo tar xj -C $samsungdir
+mkdir -p $samsungdir
+curl http://splix.ap2c.org/samsung_cms.tar.bz2 | sudo tar xj -C "$samsungdir"
 
-echo -e "$CYAN Installing canon, and epson drivers from the AUR $RESET"
+cecho "Installing canon, and epson drivers from the AUR"
 aurPackages="capt-src epson-inkjet-printer-escpr"
 trizen -S $aurPackages --needed --noconfirm
 
-echo -e "$CYAN Trizen seems to have issues installing the samsung-unified-driver, it needs to install samsung-unified-driver-common, then -printer and -scanner, then finally the base package, you might have to manually do this step after the script finishes, we install the deps first because that seems to /mostly/ work $RESET"
+cecho "Trizen seems to have issues installing the samsung-unified-driver, it needs to install samsung-unified-driver-common, then -printer and -scanner, then finally the base package, you might have to manually do this step after the script finishes, we install the deps first because that seems to /mostly/ work"
 trizen -S --asdeps samsung-unified-driver-printer --needed --noconfirm
 trizen -S samsung-unified-driver --needed --noconfirm
 
-echo -e "$CYAN Enabling / Starting CUPS $RESET"
+cecho "Enabling / Starting CUPS"
 
 sudo systemctl enable org.cups.cupsd.service
 sudo systemctl start org.cups.cupsd.service
