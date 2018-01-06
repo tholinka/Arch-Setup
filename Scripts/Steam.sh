4637b237487a38514f64d002f85591c811dba586
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2018-01-06 03:31:09 -0700

    Add required lib32 libs when installing

--- Scripts/Steam.sh
+++ Scripts/Steam.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-echo "Need 32bit version of graphics card libraries: lib32-mesa for AMD/Intel, lib32-nvidia-utils for nvidia"
-echo "Also fonts will look bad / not be there if you haven't installed the better fonts"
+SCRIPTSLOC=$(dirname "$0")
+INCLUDESLOC="$SCRIPTSLOC/includes"
 
-sudo pacman -S --needed \
-steam
+source "$INCLUDESLOC/colordefines.sh"
+
+cbecho "Installing Steam"
+
+if pacman -Q mesa &>/dev/null; then
+    cecho "Mesa is installed, installing lib32-mesa (intel/amd)"
+    INTEL="lib32-mesa"
+fi
+
+if pacman -Q nvidia-utils &>/dev/null; then
+    cecho "nvidia-utils installed, installing lib32-nvidia-utils (nvidia)"
+    NVIDIA="lib32-nvidia-utils"
+fi
+
+PACKAGES="steam"
+sudo pacman -S --needed --noconfirm \
+$PACKAGES \
+$INTEL \
+$NVIDIA
+
+
+echo;
+gbecho "Fonts will look bad / not be there if you haven't installed the better fonts"
