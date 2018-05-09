5fe5c1e9199b840a5406a14e54c791093c231beb
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2018-05-08 18:26:58 -0600

    Switch trizen -> yay

--- Scripts/includes/FirstSetup_functions/setup_aur.sh
+++ Scripts/includes/FirstSetup_functions/setup_aur.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-# get trizen if it's not installed
+# get yay if it's not installed
 function setup_aur()
 {
-    cecho "Installing trizen (aur helper)"
-    if which trizen &> /dev/null; then
-        cecho "Trizen is already installed (skipping)"
+    cecho "Installing yay (aur helper)"
+    if which yay &> /dev/null; then
+        cecho "yay is already installed (skipping)"
         return 0
     fi
 
-	cbecho "Setting up AUR (Trizen)"
+	cbecho "Setting up AUR (yay)"
 
-    cecho "Cloning Trizen's AUR repo to /tmp"
-    git clone https://aur.archlinux.org/trizen.git /tmp/trizen
-	cd /tmp/trizen
+    cecho "Cloning yay's AUR repo to /tmp"
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+	cd /tmp/yay
 
 	echo
-	cecho "Making Trizen"
+	cecho "Making yay"
 	echo
 
 	makepkg -si --noconfirm --needed
 	cd - 1>/dev/null
-	rm trizen -rf
-
-	cecho "Installing Trizen optional deps"
-	sudo pacman -S --asdeps perl-json-xs perl-term-readline-gnu
 
 	echo
-    cecho "Trizen is installed"
+    cecho "yay is installed"
 }
